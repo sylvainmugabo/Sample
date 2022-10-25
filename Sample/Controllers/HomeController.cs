9beed8648e9d52e33b8e6c06412de2b476acf661
Nkurunziza Tabazi <sylvain.tabazi@gmail.com>
2022-10-25 10:16:56 -0400

    initial project

---
+++ Sample/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sample.Models;
+using Sample.Repositories;
+
+namespace Sample.Controllers
+{
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult RsvpForm() => View();
+
+        [HttpPost]
+        public IActionResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+
+            return View();
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
---
+++ Sample/Repositories/Repository.cs
@@ -0,0 +1,15 @@
+﻿using Sample.Models;
+
+namespace Sample.Repositories
+{
+    public class Repository
+    {
+        private static List<GuestResponse> responses = new();
+        public static IEnumerable<GuestResponse> Responses => responses;
+        public static void AddResponse(GuestResponse response)
+        {
+            Console.WriteLine(response);
+            responses.Add(response);
+        }
+    }
+}
